402f272e17c70d17bf631292894cdf087ea2cac5
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-01 20:04:21 +0100

    set up vpost model and controller

--- app/controllers/vposts_controller.rb
+++ app/controllers/vposts_controller.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 
 class VpostsController < ApplicationController
   def new
   end
 
-    def create
-        render plain: params[:vpost].inspect
-      end 
-    end 
+  def create
+    render plain: params[:vpost].inspect
+    end
+end
