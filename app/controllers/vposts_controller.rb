04630ff01ea7949c60e20f5c04d8671b5b969663
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-01 22:41:15 +0100

    implemented show function

--- app/controllers/vposts_controller.rb
+++ app/controllers/vposts_controller.rb
@@ -7,7 +7,12 @@ class VpostsController < ApplicationController
   def create
     @vpost = Vpost.new(vpost_params)
     @vpost.save
+    redirect_to @vpost #gives instance var to view 
     end
+
+    def show 
+    @vpost = Vpost.find(params[:id])
+    end 
 
     def index 
     @vposts = Vpost.all 
